2c7b6a171b865b632f6890d0bcc0883c6c04b832
A7bert777 <2506245294@qq.com>
2025-09-10 14:18:30 +0000

    first commit

---
+++ build/CMakeFiles/rknn_yolov7_demo.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/src/main.cc" "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/build/CMakeFiles/rknn_yolov7_demo.dir/src/main.cc.o"
+  "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/src/postprocess.cc" "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/build/CMakeFiles/rknn_yolov7_demo.dir/src/postprocess.cc.o"
+  "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/src/yolov7.cc" "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/build/CMakeFiles/rknn_yolov7_demo.dir/src/yolov7.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../"
+  "../utils"
+  "../3rdparty/stb_image"
+  "../3rdparty/jpeg_turbo/include"
+  "../3rdparty/librga/include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/build/utils.out/CMakeFiles/imageutils.dir/DependInfo.cmake"
+  "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/build/utils.out/CMakeFiles/fileutils.dir/DependInfo.cmake"
+  "/home/firefly/GitHUb测试/YOLOv7_RK3588_object_detect/build/utils.out/CMakeFiles/imagedrawing.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
